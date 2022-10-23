4e6bfcf8507829926a9246d5f2a803be9519aa19
Sara-MF <sara.melo@aluno.uece.br>
2022-10-23 16:31:26 -0300

    tratamento de erro adicionado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ function Home() {
 
   const navigate = useNavigate(); // armazenando historico de pesquisa para mostrar na prox rota
   const [ user, setUser ] = useState('');
+  const [ error, setError ] = useState(false);
 
   function handleSearch() {
     axios.get(`https://api.github.com/users/${user}/repos`).then(response => {
@@ -18,16 +19,25 @@ function Home() {
       });
 
       localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+      setError(false); // garantia de q foi encontrado um repositório a ser buscado
       navigate('/repositories'); // rota
+    }).catch(err => {
+      setError(true);
     });
   }
 
   return (
-    <S.Container>
+    <S.HomeContainer>
       <S.Title>Repos</S.Title>
-      <S.Input className="userInput" placeholder="Usuário" value={user} onChange={e => setUser(e.target.value)} />
-      <S.Button onClick={handleSearch}>Pesquisar</S.Button>
-    </S.Container>
+
+      <S.Content>
+        <S.Input className="userInput" placeholder="Usuário" value={user} onChange={e => setUser(e.target.value)} />
+        <S.Button onClick={handleSearch}>Pesquisar</S.Button>
+      </S.Content>
+      
+      { error ? <S.ErrorMsg>Ocorreu um erro. Tente novamente</S.ErrorMsg> : '' }
+      
+    </S.HomeContainer>
   )
 }
 
